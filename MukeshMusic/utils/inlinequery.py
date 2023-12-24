8b276860a2ad544340c6f7b6c1812e6ae417fa5f
Rifkialbot2 <93379131+Rifkialbot2@users.noreply.github.com>
2023-12-25 02:06:39 +0700

    Update inlinequery.py

--- MukeshMusic/utils/inlinequery.py
+++ MukeshMusic/utils/inlinequery.py
@@ -6,39 +6,39 @@ answer = []
 answer.extend(
     [
         InlineQueryResultArticle(
-            title="🙄 ᴩᴀᴜsᴇ 🙄",
+            title="▶️ ᴩᴀᴜsᴇ ▶️",
             description=f"ᴩᴀᴜsᴇ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
-            thumb_url="https://telegra.ph/file/c5952790fa8235f499749.jpg",
+            thumb_url="https://graph.org/file/32b3d61e154df402210fc.jpg",
             input_message_content=InputTextMessageContent("/pause"),
         ),
         InlineQueryResultArticle(
-            title="😋 ʀᴇsᴜᴍᴇ 😋",
+            title="⏸️ ʀᴇsᴜᴍᴇ ⏸️",
             description=f"ʀᴇsᴜᴍᴇ ᴛʜᴇ ᴩᴀᴜsᴇᴅ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
-            thumb_url="https://telegra.ph/file/c5952790fa8235f499749.jpg",
+            thumb_url="https://graph.org/file/32b3d61e154df402210fc.jpg",
             input_message_content=InputTextMessageContent("/resume"),
         ),
         InlineQueryResultArticle(
-            title="🙂 sᴋɪᴩ 🙂",
+            title="⏪ sᴋɪᴩ ⏩",
             description=f"sᴋɪᴩ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ ᴀɴᴅ ᴍᴏᴠᴇs ᴛᴏ ᴛʜᴇ ɴᴇxᴛ sᴛʀᴇᴀᴍ.",
-            thumb_url="https://telegra.ph/file/c5952790fa8235f499749.jpg",
+            thumb_url="https://graph.org/file/32b3d61e154df402210fc.jpg",
             input_message_content=InputTextMessageContent("/skip"),
         ),
         InlineQueryResultArticle(
-            title="🥺 ᴇɴᴅ 🥺",
+            title="⏹️ ᴇɴᴅ ⏹️",
             description="ᴇɴᴅ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
-            thumb_url="https://telegra.ph/file/c5952790fa8235f499749.jpg",
+            thumb_url="https://graph.org/file/32b3d61e154df402210fc.jpg",
             input_message_content=InputTextMessageContent("/end"),
         ),
         InlineQueryResultArticle(
-            title="🥴 sʜᴜғғʟᴇ 🥴",
+            title="⏺️ sʜᴜғғʟᴇ ⏺️",
             description="sʜᴜғғʟᴇ ᴛʜᴇ ǫᴜᴇᴜᴇᴅ sᴏɴɢs ɪɴ ᴩʟᴀʏʟɪsᴛ.",
-            thumb_url="https://telegra.ph/file/c5952790fa8235f499749.jpg",
+            thumb_url="https://graph.org/file/32b3d61e154df402210fc.jpg",
             input_message_content=InputTextMessageContent("/shuffle"),
         ),
         InlineQueryResultArticle(
-            title="🥱 ʟᴏᴏᴩ 🥱",
+            title="🎦 ʟᴏᴏᴩ 🎦",
             description="ʟᴏᴏᴩ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ ᴛʀᴀᴄᴋ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
-            thumb_url="https://telegra.ph/file/c5952790fa8235f499749.jpg",
+            thumb_url="https://graph.org/file/32b3d61e154df402210fc.jpg",
             input_message_content=InputTextMessageContent("/loop 3"),
         ),
     ]
